4c34623024c49ab976eadeb4fbf5c28c98e02650
swajp <sondikemail@seznam.cz>
2022-12-05 22:47:02 +0100

    connect, create forms added

--- index.php
+++ index.php
@@ -10,10 +10,31 @@
 </head>
 <body>
 <div class="container">
-    <div class="content"
-        <a target="#"><button class="button__join">Join existing room</button></a>
-        <a target="#"><button class="button__create">Create new room</button></a>
-    </div>
+    <?php
+    if (isset($_POST['join-room'])){
+        echo "
+        <form class='content' method='post'>
+            <input class='input__black' type='text' placeholder='Enter room code...' name='' id=''>
+            <button class='button__white' name='join-room-button'>Connect to the room</button>
+        </form>
+        ";
+    }
+    else if (isset($_POST['create-room'])){
+        echo "<p>Creating room...</p>";
+    }
+    else if (isset($_POST['join-room-button'])){
+        echo "<p>Connecting to the room...</p>>";
+    }
+    else if (!isset($_POST['join-room']) || !isset($_POST['create-room'])){
+        echo "
+        <form class='content' method='post'>
+            <button class='button__black' name='join-room'>Join existing room</button>
+            <button class='button__white' name='create-room'>Create new room</button>
+        </form>
+        ";
+    }
+
+    ?>
 </div>
 </body>
 </html>
